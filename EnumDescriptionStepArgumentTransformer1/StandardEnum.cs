1928707364d4f72bd9561dd39fa74b5b882c5217
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2022-11-06 11:51:32 -0600

    added a standard enum (without Description attribute) to confirm proper handling of enums

---
+++ EnumDescriptionStepArgumentTransformer1/StandardEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDescriptionStepArgumentTransformer1
+{
+    public enum StandardEnum
+    {
+        Giant,
+        Big,
+        Normal,
+        Small
+    }
+}
